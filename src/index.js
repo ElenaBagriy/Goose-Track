59211e3de51840ff5c779533fcae95b30c171977
ElenaBagriy <elena.bagriy1989@gmail.com>
2023-08-03 13:14:00 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
       <CssBaseline />
       <BrowserRouter
-      // basename="/goose-track"
+      basename="/goose-track"
       >
         <App />
       </BrowserRouter>
